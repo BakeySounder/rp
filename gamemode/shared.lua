2460a6e920282dd7b3dfa94880f0d618900435d0
BakeySounder <65306613+BakeySounder@users.noreply.github.com>
2020-06-08 14:24:51 +0300

    Update shared.lua

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -1,50 +1,33 @@
 AddCSLuaFile()
 
-GM.Name 		= ""
-GM.Author 		= ""
+GM.Name 		= "Gmod Role Play"
+GM.Author 		= "Bakey Sounder"
 GM.Email 		= ""
 GM.Website 		= ""
-
+DeriveGamemode('sandbox')
 function recursiveInclusion( scanDirectory, isGamemode )
-	-- Null-coalescing for optional argument
 	isGamemode = isGamemode or false
-	
 	local queue = { scanDirectory }
-	
-	-- Loop until queue is cleared
 	while #queue > 0 do
-		-- For each directory in the queue...
 		for _, directory in pairs( queue ) do
-			-- print( "Scanning directory: ", directory )
-			
+			 print( "Сканирование папки: ", directory )
 			local files, directories = file.Find( directory .. "/*", "LUA" )
-			
-			-- Include files within this directory
 			for _, fileName in pairs( files ) do
-				if fileName != "shared.lua" and fileName != "init.lua" and fileName != "cl_init.lua" then
-					-- print( "Found: ", fileName )
-					
-					-- Create a relative path for inclusion functions
-					-- Also handle pathing case for including gamemode folders
+				if fileName ~= "shared.lua" and fileName ~= "init.lua" and fileName ~= "cl_init.lua" then
 					local relativePath = directory .. "/" .. fileName
 					if isGamemode then
 						relativePath = string.gsub( directory .. "/" .. fileName, GM.FolderName .. "/gamemode/", "" )
 					end
-					
-					-- Include server files
+					print( "Найдено: ", relativePath )
 					if string.match( fileName, "^sv" ) then
 						if SERVER then
 							include( relativePath )
 						end
 					end
-					
-					-- Include shared files
 					if string.match( fileName, "^sh" ) then
 						AddCSLuaFile( relativePath )
 						include( relativePath )
 					end
-					
-					-- Include client files
 					if string.match( fileName, "^cl" ) then
 						AddCSLuaFile( relativePath )
 						
@@ -54,14 +37,10 @@ function recursiveInclusion( scanDirectory, isGamemode )
 					end
 				end
 			end
-			
-			-- Append directories within this directory to the queue
 			for _, subdirectory in pairs( directories ) do
-				-- print( "Found directory: ", subdirectory )
+				print( "Найдена папка: ", subdirectory )
 				table.insert( queue, directory .. "/" .. subdirectory )
 			end
-			
-			-- Remove this directory from the queue
 			table.RemoveByValue( queue, directory )
 		end
 	end
